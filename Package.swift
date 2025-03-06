2184dc99c420be42581d58bd30beef392a5ab969
kyungboshim <kyungbo.shim@neowiz.com>
2025-03-06 16:21:37 +0900

    test

--- Package.swift
+++ Package.swift
@@ -7,14 +7,23 @@ let package = Package(
     name: "Adiscope",
     platforms: [.iOS(.v12)],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Adiscope",
-            targets: ["Adiscope"]),
+            type: .dynamic,
+            targets: ["AdiscopeTarget"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AdiscopeTarget",
+            dependencies: ["Adiscope"]
+        ),
         .binaryTarget(
           name: "Adiscope",
-          path: "./Framework/Adiscope.xcframework"
+          url: "https://github.com/adiscope/Adiscope-iOS-SPM-Test/releases/download/0.0.6/Adiscope.xcframework.zip",
+          checksum: "2fa896d87168b70edc8edfb4a53a8f1771cda997a5d75a6135f260d5fbaf13f4"
         ),
     ]
 )
