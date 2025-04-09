1fad4d5eae6e1df3b3cd89d4d598a386dc8ab769
kyungboshim <kyungbo.shim@neowiz.com>
2025-04-09 16:56:59 +0900

    test

--- Package.swift
+++ Package.swift
@@ -9,30 +9,13 @@ let package = Package(
     products: [
         .library(
             name: "Adiscope",
-            targets: ["AdiscopeTarget"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/adiscope/Adiscope-iOS-Pangle.git", exact: "0.0.1"),
+            targets: ["Adiscope"]),
     ],
     targets: [
-        .target(
-          name: "AdiscopeTarget",
-          dependencies: [
-            .target(name: "Adiscope"),
-            .target(name: "AdiscopeMediaPangle"),
-            .product(name: "AdiscopeWithPangle", package: "Adiscope-iOS-Pangle"),
-          ],
-          path: "Sources"
-        ),
         .binaryTarget(
           name: "Adiscope",
-          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.17/Adiscope.xcframework.zip",
-          checksum: "87db5fff5a34db747ff980bac344a92ec8483297b9264838ec076eb4bfcf13ee"
-        ),
-        .binaryTarget(
-          name: "AdiscopeMediaPangle",
-          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.0.18/AdiscopeMediaPangle.xcframework.zip",
-          checksum: "2fdfe52f0158913d0d9e49f69daf3bcaf0dafb1b381a791caa3da29436d4f160"
+          url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.1.0/Adiscope.zip",
+          checksum: "6141c4b42b9918dc2fd03c37de639b08cfdf50c03a60d9a2e5995447abc52758"
         ),
     ]
 )
