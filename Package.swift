0b4dd86fb1be0af86ef2b2566aa60e1854e01dcd
kyungboshim <kyungbo.shim@neowiz.com>
2025-09-08 18:21:39 +0900

    골프존 합본 전달용

--- Package.swift
+++ Package.swift
@@ -9,13 +9,132 @@ let package = Package(
     products: [
         .library(
             name: "Adiscope",
-            targets: ["Adiscope"]),
+            targets: ["AdiscopeTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/adiscope/Adiscope-iOS-ChartBoost.git", exact: "4.3.0"),
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.1.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.2.0"),
+        .package(url: "https://github.com/adiscope/Adiscope-iOS-Pangle.git", exact: "4.2.1"),
     ],
     targets: [
+        .target(
+            name: "AdiscopeTarget",
+            dependencies: [
+                .target(name: "Adiscope"),
+                .target(name: "AdiscopeMediaChartBoost"),
+                .product(name: "AdiscopeWithChartBoost", package: "Adiscope-iOS-ChartBoost"),
+                .target(name: "AdiscopeMediaMax"),
+                .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package"),
+                .target(name: "AdiscopeMediaMaxAdapterAdMob"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .target(name: "AdiscopeMediaMaxAdapterBidMachine"),
+                .target(name: "BidMachine"),
+                .target(name: "StackModules"),
+                .target(name: "StackProductPresentation"),
+                .target(name: "StackRendering"),
+                .target(name: "OMSDK_Appodeal"),
+                .target(name: "AdiscopeMediaMaxAdapterDTExchange"),
+                .target(name: "IASDKCore"),
+                .target(name: "AdiscopeMediaMaxAdapterMoloco"),
+                .target(name: "MolocoSDK"),
+                .target(name: "AdiscopeMediaMaxAdapterUnityAds"),
+                .target(name: "UnityAds"),
+                .target(name: "AdiscopeMediaPangle"),
+                .product(name: "AdiscopeWithPangle", package: "Adiscope-iOS-Pangle"),
+                .target(name: "TnkRwdSdk2"),
+            ],
+            path: "Sources"
+        ),
         .binaryTarget(
           name: "Adiscope",
           url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.1.11/Adiscope.zip",
           checksum: "2803d4643ea767da9c77f2f7e78bfed28d457cf0e99beea68c5dfdc966ce1f9f"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaChartBoost",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.3.0/AdiscopeMediaChartBoost.zip",
+            checksum: "3078974d7332252a376776d47a2b49d3e7aa4801c63aaf3f9a410617a9c5600b"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaMax",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.3.0/AdiscopeMediaMax.zip",
+            checksum: "51bf4b20477d8a2c7cc6b1187701492b03bb01f69abf2ba65fcc3c92cf653c81"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaMaxAdapterAdMob",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.3.0/AdiscopeMediaMaxAdapterAdMob.zip",
+            checksum: "a2b9727411a0cc1d3db798a3a80aab3a96ff5a4cbe451f211dc47e1b6856fcf1"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaMaxAdapterBidMachine",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.3.0/AdiscopeMediaMaxAdapterBidMachine.zip",
+            checksum: "a8151a72e44616580a342a21f2f7ed97f771f68e3e417f92e68f45fe832f5891"
+        ),
+        .binaryTarget(
+            name: "BidMachine",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/BidMachine.xcframework.zip",
+            checksum: "0bed28eec0a194a8e6b99bd5c178b459024a95ad4c83be1bb1b949579c038c0e"
+        ),
+        .binaryTarget(
+            name: "StackModules",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/StackModules.xcframework.zip",
+            checksum: "55c3ffcbf330c581afb2290c615441eff9c72d44f5b5ee30ed987f342cc43e77"
+        ),
+        .binaryTarget(
+            name: "StackProductPresentation",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/StackProductPresentation.xcframework.zip",
+            checksum: "c14278e0945b200afdf3864cae289a14b65a8e9c301b657caae1d5bb5ef06905"
+        ),
+        .binaryTarget(
+            name: "StackRendering",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/StackRendering.xcframework.zip",
+            checksum: "436a5aea2c46ac3fc06e5e901dad6a7df702564d3ceefb4ee8fd0e65947ad5e6"
+        ),
+        .binaryTarget(
+            name: "OMSDK_Appodeal",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/OMSDK_Appodeal.xcframework.zip",
+            checksum: "8b03911438e9bee6fb36aa56a29008abc2ac91fd37af4ca0d99f13ade25caccf"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaMaxAdapterDTExchange",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.3.0/AdiscopeMediaMaxAdapterDTExchange.zip",
+            checksum: "20c2fd8417b3b44d0d23a45cb079a1eea3fc331b9aae832c75528c873b7f143e"
+        ),
+        .binaryTarget(
+            name: "IASDKCore",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/IASDKCore.xcframework.zip",
+            checksum: "85d11954350be3f4ca9feefa3b6dc101c293f9224eda254b4c0b54f647ad5516"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaMaxAdapterMoloco",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.3.0/AdiscopeMediaMaxAdapterMoloco.zip",
+            checksum: "656ac474d5b80cd58e33cef5170433079cf52f3d5ad4176f6bc7dfdc6059e8ae"
+        ),
+        .binaryTarget(
+            name: "MolocoSDK",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/MolocoSDK.xcframework.zip",
+            checksum: "764bb21a59487831ec167e813150c3b2aeb6256c73aea84dc60eac754d7d91b3"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaMaxAdapterUnityAds",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.3.0/AdiscopeMediaMaxAdapterUnityAds.zip",
+            checksum: "b2e70de73a631a30c6b6311baf798a787f472beb3df8aae8c0430631cbd98352"
+        ),
+        .binaryTarget(
+            name: "UnityAds",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/UnityAds.xcframework.zip",
+            checksum: "1f482a2dbb154abf881e524de103a1bf64b7a426b7bd07d615d6fdd61bcea5fb"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaPangle",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.3.0/AdiscopeMediaPangle.zip",
+            checksum: "937395191ca8f44acb746fa01339c09315a07aff0dc7f0f1fadf046ed7ba14b3"
+        ),
+        .binaryTarget(
+            name: "TnkRwdSdk2",
+            url: "https://github.com/adiscope/Adiscope-iOS-Developer/releases/download/2.1.11/TnkRwdSdk2.v.5.73.zip",
+            checksum: "a98ef1ba9af1c5cb06b88cc0968f8f80407d36320707db7d6ff8d7cf6e847947"
         ),
     ]
 )
